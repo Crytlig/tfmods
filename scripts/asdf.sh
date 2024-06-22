77139256d15ca787f131b8a1f371de535e481e27
Claes Rytlig <crytlig@hotmail.com>
2024-06-22 15:53:16 +0200

    Add tflint and update stucture

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -4,5 +4,5 @@ asdf plugin add terragrunt
 asdf plugin add terraform-docs
 asdf plugin add terramate
 asdf plugin add gum
-
+asdf plugin add tflint
 asdf install
--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,16 +1,12 @@
-#!/usr/bin/bash
+#!/bin/bash
 set -e
 
 echo "--> Generating doc"
 rm -f .terraform.lock.hcl
 
-if [ -f ".terraform-docs.yml" ]; then
-	terraform-docs -c .terraform-docs.yml --output-file README.md ./
-elif [ -d ".terraform-docs" ]; then
-  configs=$(find .terraform-docs -maxdepth 1 -mindepth 1 -type f)
-  for config in $configs; do
-    terraform-docs -c $config ./
-  done
-else
-	terraform-docs markdown table --output-file README.md --output-mode inject ./
-fi
+mods=$(find modules/* -type d -maxdepth 0)
+for mod in $mods; do
+  pushd $mod
+  terraform-docs -c .terraform-docs.yml .
+  popd
+done
