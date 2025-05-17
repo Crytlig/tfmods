e9ec0d8e0bf3685248ae1b1347c0fbfbc03a4ece
Claes Rytlig <crytlig@hotmail.com>
2025-05-17 21:59:28 +0200

    Change source

--- modules/resource_group/examples/default/main.tf
+++ modules/resource_group/examples/default/main.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
-  source = "github.com/crytlig/tfmods//modules/resource_group?ref=main"
-  # source = "../../"
+  # source = "github.com/crytlig/tfmods//modules/resource_group?ref=main"
+  source = "../../"
 
   name     = "rg-workload"
   location = "westeurope"
