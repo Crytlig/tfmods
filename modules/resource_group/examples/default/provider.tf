9650beb5b4d7f780d21775c685cd49250906708a
Claes Rytlig <crytlig@hotmail.com>
2025-05-25 23:42:15 +0200

    Update provider versions

--- modules/resource_group/examples/default/provider.tf
+++ modules/resource_group/examples/default/provider.tf
@@ -3,11 +3,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.85.0"
+      version = "~> 4.0.0"
     }
   }
 }
 provider "azurerm" {
   features {}
 }
- 
+
+
--- modules/virtual_machine/examples/default/provider.tf
+++ modules/virtual_machine/examples/default/provider.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.85.0"
+      version = "~> 4.0"
     }
   }
 }
@@ -11,3 +11,4 @@ provider "azurerm" {
   features {}
 }
  
+
