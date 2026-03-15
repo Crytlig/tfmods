f9cf9f6c7d9f92073396187a07c02bc5f3f71214
Claes Rytlig <crytlig@hotmail.com>
2026-03-15 19:36:02 +0100

    style(private_endpoint): add output descriptions

--- modules/private_endpoint/outputs.tf
+++ modules/private_endpoint/outputs.tf
@@ -1,15 +1,19 @@
 output "id" {
-  value = azurerm_private_endpoint.this.id
+  description = "The ID of the private endpoint."
+  value       = azurerm_private_endpoint.this.id
 }
 
 output "custom_dns_configs" {
-  value = azurerm_private_endpoint.this.custom_dns_configs
+  description = "The custom DNS configurations of the private endpoint."
+  value       = azurerm_private_endpoint.this.custom_dns_configs
 }
 
 output "subnet_id" {
-  value = azurerm_private_endpoint.this.subnet_id
+  description = "The ID of the subnet associated with the private endpoint."
+  value       = azurerm_private_endpoint.this.subnet_id
 }
 
 output "network_interface" {
-  value = azurerm_private_endpoint.this.network_interface
+  description = "The network interface associated with the private endpoint."
+  value       = azurerm_private_endpoint.this.network_interface
 }
